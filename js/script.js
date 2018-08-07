d76d1460e5dc605702aaf4be0dcb58fe4f745972
Xavier <parsonsxe@live.com>
2018-08-07 15:33:59 -0400

    update

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Add variables that store DOM elements you will need to reference and/or manipulate
+// variables to manipulate DOM elements
 const listContainer = document.getElementsByClassName('.student-list');
 const studentSelector = document.querySelectorAll('li');
-let page;
+const pageContainer = document.getElementsByClassName('.page')
 
 
-// hideList function hides all students from the page
+// function to hide all students from page
 const hideList = (studentSelector) => {
 	for (let i = 0; i < studentSelector.length; i++) {
 		studentSelector[i].style.display = 'none';
@@ -17,7 +17,7 @@ const hideList = (studentSelector) => {
 };
 
 
-// Create a function to hide all of the items in the list except for the ten you want to show
+// function that shows only the first ten students
 const showPage = (page, studentSelector) => {
 	hideList(studentSelector);
 	for (let i = 0; i < studentSelector.length; i++) {
@@ -32,11 +32,27 @@ showPage(1, studentSelector);
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
 
+const createLinkDiv = () => {
+	let ul = document.querySelector('ul');
+	let li = document.createElement('a', 'li');
+	li.className = 'pagination';
+	li.innerHTML = 1;
+	ul.appendChild(li);
+};
+
+
+const appendPageLinks = (studentSelector) => {
+	let pages = Math.ceil(studentSelector.length / 10);
+	for (var i = 0; i <= pages; i++) {
+		
+	}
+};
+
+appendPageLinks(1,studentSelector);
+
+
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
-
-
-
 
 
 
