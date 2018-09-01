138992ed90700297343488d23731c89c576e7c25
Xavier <parsonsxe@live.com>
2018-09-01 14:58:06 -0400

    updates

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ FSJS project 2 - List Filter and Pagination
 const listContainer = document.getElementsByClassName('student-list');
 const studentSelector = document.querySelectorAll('li');
 const pageContainer = document.getElementsByClassName('page');
-// function to hide all students from page
 
+// function to hide all students from page
 const hideList = (studentSelector) => {
 	for (let i = 0; i < studentSelector.length; i++) {
 		studentSelector[i].style.display = 'none';
@@ -19,7 +19,6 @@ const hideList = (studentSelector) => {
 
 
 // function that shows only the first ten students by default
-
 const showPage = (page, studentSelector) => {
 	hideList(studentSelector);
 	let showStudentList = ( page * 10 );
@@ -34,7 +33,6 @@ const showPage = (page, studentSelector) => {
 
 
 // Create and append the pagination links 
-
 const appendPageLinks = (studentSelector) => {
 	let pages = Math.ceil(studentSelector.length / 10);
 	for (let i = 0; i < pages; i++) {
@@ -44,22 +42,24 @@ const appendPageLinks = (studentSelector) => {
 
 
 const createLinkDiv = (pageNum) => {    
-    
 	let ul = document.querySelector('ul');
 	let li = document.createElement('li');
 	ul.className='pagination';
 	li.innerHTML = `<a href="#">${pageNum}</a>`;
 	ul.appendChild(li);
 	let pageLinks = ul.lastChild;
+    
 	// Listens for click on the page links div and assigns pageNum value to showPage
 	pageLinks.addEventListener('click', () => {
 		showPage(pageNum,studentSelector);
+        
 	});
 };
 
 
 appendPageLinks(studentSelector);
 showPage(1, studentSelector);
+
 
 let anchors = document.querySelector('a');
 anchors.className = 'active';
